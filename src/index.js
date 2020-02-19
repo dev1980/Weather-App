68f198e61b4ccad76c7b34c2d84d04928fd46670
dev1980 <brham1980@gmail.com>
2020-02-19 18:00:01 +0545

    dom manipulation

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 import './style.css';
 
-let cityInput = document.querySelector('.cityInput');
-let button = document.querySelector('.button');
+const cityInput = document.querySelector('.cityInput');
+const button = document.querySelector('.button');
 
-let date = document.querySelector('.date');
-let city = document.querySelector('.city');
-let temp = document.querySelector('.temp');
-let tempHigh = document.querySelector('.tempHigh');
-let tempLow = document.querySelector('.tempLow');
-let wind = document.querySelector('.wind');
-let humidity = document.querySelector('.humidity');
-let cloud = document.querySelector('.cloud');
+const city = document.querySelector('.city');
+const temp = document.querySelector('.temp');
+const tempHigh = document.querySelector('.tempHigh');
+const tempLow = document.querySelector('.tempLow');
+const wind = document.querySelector('.wind');
+const cloud = document.querySelector('.cloud');
 
 button.addEventListener('click', function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityInput.value + '&appid=80068fbfc8f92310ac9ea24006b4d8f7')
     .then(response => response.json())
-    .then(data => 
-        console.log(data)
-    )
+    .then(data => {
+        const cityName = data['name'];
+        const tempValue = data['main']['temp'];
+        const tempHighValue = data['main']['temp_max'];
+        const tempLowValue = data['main']['temp_min'];
+        const windValue = data['wind']['speed'];
+        const cloudValue = data['clouds']['all'];
+       
+
+        city.innerHTML = cityName;
+        temp.innerHTML = tempValue;
+        tempHigh.innerHTML = tempHighValue;
+        tempLow.innerHTML = tempLowValue;
+        wind.innerHTML = windValue;
+        cloud.innerHTML = cloudValue;
+        
+    })
     
     .catch(error => alert("Wrong city name"))
 })
