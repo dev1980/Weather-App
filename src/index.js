a6ce3a28130925bfe8996db979dfa827bb82bb8f
dev1980 <brham1980@gmail.com>
2020-02-19 17:23:35 +0545

    add function to load weather api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css';
+
+let cityInput = document.querySelector('.cityInput');
+let button = document.querySelector('.button');
+
+let date = document.querySelector('.date');
+let city = document.querySelector('.city');
+let temp = document.querySelector('.temp');
+let tempHigh = document.querySelector('.tempHigh');
+let tempLow = document.querySelector('.tempLow');
+let wind = document.querySelector('.wind');
+let humidity = document.querySelector('.humidity');
+let cloud = document.querySelector('.cloud');
+
+button.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityInput.value + '&appid=80068fbfc8f92310ac9ea24006b4d8f7')
+    .then(response => response.json())
+    .then(data => 
+        console.log(data)
+    )
+    
+    .catch(error => alert("Wrong city name"))
+})
