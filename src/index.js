f53fa011b06a507d02df52d1deb5446d3ebf92fe
dev1980 <brham1980@gmail.com>
2020-02-25 17:59:53 +0545

    change function to es6 and also add default image and implementimage display for each weather

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './style.css';
-
+import image from '../image/cloud.jpg';
 const btnCity = document.getElementById('btnCity');
 const txtCity = document.getElementById('txtCity');
-const imgGif = document.getElementById('imgGif');
 const btnCelsius = document.getElementById('btnCelsius');
 const btnFahren = document.getElementById('btnFahren');
 const location = document.getElementById('location');
@@ -11,20 +10,21 @@ const humidity = document.getElementById('humidity');
 const pressure = document.getElementById('pressure');
 const wind = document.getElementById('wind');
 const weather = document.getElementById('weather');
+const imgGif = document.getElementById('imgGif');
 
-const displayImage = () => {
-  fetch('https://api.giphy.com/v1/gifs/random?api_key=UATrdJQ14gXYSQb46ecz4AExyXbN27Qn&tag=&rating=G')
+const displayImage = (gifyimage) => {
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=tF7lrMLP7aaUUr1PIcT4fubRikdBCMWU&s=${gifyimage}%20cloudy`)
     .then(response => response.json())
     .then((response) => {
-      document.querySelector('img').src = response.data.images.original.url;
+      imgGif.src = response.data.images.original.url;
     })
     .catch((error) => {
       document.querySelector('error').style.display = 'block';
       document.querySelector('error').innerHTML = error;
+      
     });
 };
-
-imgGif.innerHTML = displayImage();
+imgGif.src =image;
 
 let data;
 btnCity.onclick = () => {
@@ -35,14 +35,17 @@ btnCity.onclick = () => {
     response.json().then(json => {
       data = json;
       getResponse(data);
+
+    const gifyimage = data.weather[0].main;
+     displayImage(gifyimage.toLowerCase());
     });
   });
 };
 
-function kToC(kTemp) {
+const kToC = (kTemp) => {
   const cTemp = kTemp - 273.15;
   return cTemp;
-}
+};
 
 const getResponse = (data) => {
   let conditions = '';
@@ -56,21 +59,23 @@ const getResponse = (data) => {
   } else {
     conditions += data.weather[0].main;
   }
+  
   location.innerHTML = `Current weather condition for ${data.name}`;
-  changeTemp.innerHTML = `${Math.round(kToC(data.main.temp))}C`;
+  changeTemp.innerHTML = `${Math.round(kToC(data.main.temp))} °C`;
   humidity.innerHTML = `${data.main.humidity}%`;
   pressure.innerHTML = `${data.main.pressure}mb`;
   wind.innerHTML = `${data.wind.deg} degrees at ${(data.wind.speed)}ms`;
   weather.innerHTML = `${conditions}`;
+  
 };
 
 btnCelsius.addEventListener('click', () => {
   const cels = Math.round(kToC(data.main.temp));
-  changeTemp.innerHTML = `${cels}C`;
+  changeTemp.innerHTML = `${cels} °C`;
 });
 
 btnFahren.addEventListener('click', () => {
   const cels = Math.round(kToC(data.main.temp));
   const fahren = (cels * (9 / 5)) + 32;
-  changeTemp.innerHTML = `${fahren}F`;
+  changeTemp.innerHTML = `${fahren} °F`;
 });
